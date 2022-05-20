c1df12ff6ba2c3702dee8a2a5256d0518f9c4215
axfbj <1134281225@qq.com>
2022-05-21 00:07:33 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,10 @@ const handlerClick = () => {
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <InputNumber />
-  <n-button @click="handlerClick">{{ number }}</n-button>
+  <n-button @click="handlerClick">Default: {{ number }}</n-button>
   <n-button type="primary"> Primary </n-button>
   <n-button type="info"> Info </n-button>
-  <n-button type="success"> </n-button>
+  <n-button type="success">Success</n-button>
   <n-button type="warning"> Warning </n-button>
   <n-button type="error"> Error </n-button>
 </template>
