8b313e26f976f25a6391ea816a0ab3c3b1a9573a
axfbj <1134281225@qq.com>
2022-05-21 11:58:09 +0800

    精简配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,22 +14,11 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
-  // plugins: [
-  //   // 用到的插件
-  //   '@typescript-eslint',
-  //   'prettier'
-  // ],
-  // globals: {
-  //   PublicKeyCredential: true
-  // },
   rules: {
-    // "no-unused-vars": "off",
     '@typescript-eslint/no-unused-vars': ['off'],
-    'vue/script-setup-uses-vars': 'error', // setup 语法糖校验
-
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // "prettier/prettier": "error", // prettier标记的地方抛出错误信息
+    'spaced-comment': [2, 'always'], // 注释后面必须写两个空格
     'prettier/prettier': [
       'warn',
       {
@@ -42,8 +31,7 @@ module.exports = {
         trailingComma: 'none',
         bracketSpacing: true
       }
-    ],
-    'spaced-comment': [2, 'always'] // 注释后面必须写两个空格
+    ]
     // "@typescript-eslint/no-explicit-any": ["off"] // 关闭any校验,"
   }
 }
